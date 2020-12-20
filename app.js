689f3954cd63ad75404aa31f9f3c00323a85456f
Nick De Marco <nickdmrco@gmail.com>
2020-12-20 12:22:18 -0800

    touches

--- app.js
+++ app.js
@@ -383,3 +383,310 @@ const deleteRole = () => {
       }
     })
   }
+
+  const updateMenu = () => {
+    db.query('SELECT * FROM employees', (err, employees) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.table(employees)
+        inquirer.prompt([
+          {
+            type: 'input',
+            name: 'id',
+            message: 'Enter the id of the employee you wish to edit (leave blank to return to the main menu):',
+            validate: function (res) {
+              let id = parseInt(res)
+              return (!isNaN(id) && id > 0) || (res === '')
+            }
+          }
+        ])
+          .then(({ id }) => {
+            if (id === '') {
+              mainMenu()
+            } else {
+              updateEmployee(id)
+            }
+          })
+          .catch(err => console.log(err))
+      }
+    })
+  }
+
+  const addEmployee = () => {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'firstName',
+        message: 'What is their First Name:'
+      },
+      {
+        type: 'input',
+        name: 'lastName',
+        message: 'What is their Last Name:'
+      },
+      {
+        type: 'input',
+        name: 'roleId',
+        message: `What is their role ID:`,
+        validate: function (res) {
+          return !isNaN(parseInt(res))
+        }
+      },
+      {
+        type: 'input',
+        name: 'managerId',
+        message: `Enter their manager's id (leave blank if they don't have one):`,
+        validate: function (res) {
+          return (res === '' || !isNaN(parseInt(res)))
+        }
+      }
+    ])
+      .then(employee => {
+        if (employee.managerId === '') {
+          employee.managerId = null
+        }
+        db.query(`INSERT INTO employees SET ?`, employee, err => {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log('Employee Added!')
+          }
+          inquirer.prompt([
+            {
+              type: 'list',
+              name: 'option',
+              message: 'What would you like to do next:',
+              choices: ['Add Another Employee', 'Return to Add Menu', 'Return to Main Menu']
+            }
+          ])
+            .then(({ option }) => {
+              switch (option) {
+                case 'Add Another Employee':
+                  addEmployee()
+                  break
+                case 'Return to Add Menu':
+                  addMenu()
+                  break
+                case 'Return to Main Menu':
+                  mainMenu()
+                  break
+              }
+            })
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  const addRole = () => {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Give the new role a title:'
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: `What is the role's yearly salary in USD:`,
+        validate: function (res) {
+          return !isNaN(parseInt(res))
+        }
+      },
+      {
+        type: 'input',
+        name: 'departmentId',
+        message: 'Please enter the department Id for this role:',
+        validate: function (res) {
+          return !isNaN(parseInt(res))
+        }
+      }
+    ])
+      .then(role => {
+        db.query(`INSERT INTO roles SET ?`, role, err => {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log('Role Added!')
+          }
+          inquirer.prompt([
+            {
+              type: 'list',
+              name: 'option',
+              message: 'What would you like to do next:',
+              choices: ['Add Another Role', 'Return to Add Menu', 'Return to Main Menu']
+            }
+          ])
+            .then(({ option }) => {
+              switch (option) {
+                case 'Add Another Role':
+                  addRole()
+                  break
+                case 'Return to Add Menu':
+                  addMenu()
+                  break
+                case 'Return to Main Menu':
+                  mainMenu()
+                  break
+              }
+            })
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  const addDepartment = () => {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Please give the new department a name:'
+      }
+    ])
+      .then(department => {
+        db.query(`INSERT INTO departments SET ?`, department, err => {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log('Department Added!')
+          }
+          inquirer.prompt([
+            {
+              type: 'list',
+              name: 'option',
+              message: 'What would you like to do next:',
+              choices: ['Add Another Department', 'Return to Add Menu', 'Return to Main Menu']
+            }
+          ])
+            .then(({ option }) => {
+              switch (option) {
+                case 'Add Another Department':
+                  addDepartment()
+                  break
+                case 'Return to Add Menu':
+                  addMenu()
+                  break
+                case 'Return to Main Menu':
+                  mainMenu()
+                  break
+              }
+            })
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  const addMenu = () => {
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'option',
+        message: 'Which of the following would you like to add:',
+        choices: ['Departments', 'Roles', 'Employees', 'Return to Main Menu']
+      }
+    ])
+      .then(({ option }) => {
+        switch (option) {
+          case 'Departments':
+            addDepartment()
+            break
+          case 'Roles':
+            addRole()
+            break
+          case 'Employees':
+            addEmployee()
+            break
+          case 'Return to Main Menu':
+            mainMenu()
+            break
+        }
+      })
+  }
+
+  const viewMenu = () => {
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'option',
+        message: 'Which of the following would you like to view:',
+        choices: ['Departments', 'Roles', 'Employees', 'Return to Main Menu']
+      }
+    ])
+      .then(({ option }) => {
+        switch (option) {
+          case 'Departments':
+            db.query('SELECT * FROM departments', (err, departments) => {
+              if (err) {
+                console.log(err)
+              } else {
+                console.table(departments.sort((a, b) => { return a.id - b.id }))
+                viewMenu()
+              }
+            })
+            break
+          case 'Roles':
+            db.query('SELECT * FROM roles', (err, roles) => {
+              if (err) {
+                console.log(err)
+              } else {
+                console.table(roles.sort((a, b) => { return a.id - b.id }))
+                viewMenu()
+              }
+            })
+            break
+          case 'Employees':
+            db.query('SELECT * FROM employees', (err, employees) => {
+              if (err) {
+                console.log(err)
+              } else {
+                console.table(employees.sort((a, b) => { return a.id - b.id }))
+                viewMenu()
+              }
+            })
+            break
+          case 'Return to Main Menu':
+            mainMenu()
+            break
+        }
+      })
+      .catch(err => console.log(err))
+  }
+
+  const mainMenu = () => {
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'option',
+        message: 'What would you like to do:',
+        choices: [
+          'Add departments, roles, employees',
+          'View departments, roles, employees',
+          'Update employee roles',
+          'Delete departments, roles, and employees',
+          'EXIT'
+        ]
+      }
+    ])
+      .then(({ option }) => {
+        switch (option) {
+          case 'Add departments, roles, employees':
+            addMenu()
+            break
+          case 'View departments, roles, employees':
+            viewMenu()
+            break
+          case 'Update employee roles':
+            updateMenu()
+            break
+          case 'Delete departments, roles, and employees':
+            deleteMenu()
+            break
+          case 'EXIT':
+            process.exit()
+            break
+        }
+      })
+      .catch(err => console.log(err))
+  }
+
+  mainMenu()
